743dda50301b4ab4a604f7fbe7c5ad428a85445d
Jason Jerome <jjerome@generalui.com>
2016-10-03 21:26:34 -0700

    Added UI test for WelcomeActivityUITest

--- app/src/androidTest/java/com/bikefit/wedgecalculator/main/WelcomeActivityUITest.java
+++ app/src/androidTest/java/com/bikefit/wedgecalculator/main/WelcomeActivityUITest.java
@@ -1,14 +1,25 @@
 package com.bikefit.wedgecalculator.main;
 
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.bikefit.wedgecalculator.R;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
- * Created by max on 10/3/16.
+ * UI Test for the WelcomeActivity
  */
+@RunWith(AndroidJUnit4.class)
 public class WelcomeActivityUITest {
 
     //region CLASS UNDER TEST ----------------------------------------------------------------------
@@ -42,18 +53,19 @@ public class WelcomeActivityUITest {
 
     //region TESTS ---------------------------------------------------------------------------------
 
-/*
-    @Ignore
+
     @Test
-    public void testHelloWorldText() {
+    public void testOptionButtonsAppear() {
         // WHEN the activity starts
         mActivityRule.launchActivity(null);
 
+        // THEN the 3 options appear as buttons
+        onView(withId(R.id.activity_welcome_more_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.activity_welcome_orientation_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.activity_welcome_start_button)).check(matches(isDisplayed()));
 
-        // THEN the hello world text is displayed
-        onView(withText("Bikefit")).check(matches(isDisplayed()));
     }
-*/
+
 
     //endregion
 
