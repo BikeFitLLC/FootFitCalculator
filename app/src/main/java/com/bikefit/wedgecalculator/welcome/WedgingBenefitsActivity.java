13616911784d0ece3251c07d697673e9defcc626
Jason Jerome <jjerome@generalui.com>
2016-10-04 14:23:24 -0700

    Add link between WedgingBenefits (via button) and the Main Menu Activity

--- app/src/main/java/com/bikefit/wedgecalculator/welcome/WedgingBenefitsActivity.java
+++ app/src/main/java/com/bikefit/wedgecalculator/welcome/WedgingBenefitsActivity.java
@@ -1,5 +1,6 @@
 package com.bikefit.wedgecalculator.welcome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,16 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.bikefit.wedgecalculator.R;
+import com.bikefit.wedgecalculator.main.MainMenuActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Activity for the Wedging Benefits screen
+ * This is a navigation endpoint, so it's just an activity
  */
 public class WedgingBenefitsActivity extends AppCompatActivity {
+
+    // region LIFECYCLE METHODS --------------------------------------------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wedging_benefits);
+        ButterKnife.bind(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -27,6 +36,8 @@ public class WedgingBenefitsActivity extends AppCompatActivity {
         }
     }
 
+    // endregion
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -34,4 +45,14 @@ public class WedgingBenefitsActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    // region LISTENER METHODS ---------------------------------------------------------------------
+
+    @OnClick(R.id.activity_wedging_benefits_start_button)
+    public void onGetStartedButton() {
+        startActivity(new Intent(WedgingBenefitsActivity.this, MainMenuActivity.class));
+        finish();
+    }
+
+    // endregion
 }
