5cf09a8778d91212a86f0639061d9340c248fa66
Jason Jerome <jjerome@generalui.com>
2016-10-03 21:29:03 -0700

    Update layout for WedgingBenefitsActivity to get the toolbar working

--- app/src/main/java/com/bikefit/wedgecalculator/main/WedgingBenefitsActivity.java
+++ app/src/main/java/com/bikefit/wedgecalculator/main/WedgingBenefitsActivity.java
@@ -1,12 +1,15 @@
 package com.bikefit.wedgecalculator.main;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.bikefit.wedgecalculator.R;
 
 /**
- * Created by GeneralUI on 10/3/16.
+ * Activity for the Wedging Benefits screen
  */
 public class WedgingBenefitsActivity extends AppCompatActivity {
 
@@ -14,5 +17,21 @@ public class WedgingBenefitsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wedging_benefits);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            super.onBackPressed();
+        }
+        return true;
     }
 }
