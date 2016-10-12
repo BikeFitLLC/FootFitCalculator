0ee8afa9a81331faaaf0ddaa90db876e63f0e628
Jason Jerome <jjerome@generalui.com>
2016-10-12 10:01:17 -0700

    Scale bitmap according to scale factor calculation.  Moved calculation to background thread.

--- app/src/main/java/com/bikefit/wedgecalculator/camera/MeasurementFragment.java
+++ app/src/main/java/com/bikefit/wedgecalculator/camera/MeasurementFragment.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -27,7 +28,6 @@ public class MeasurementFragment extends Fragment {
     //region STATIC LOCAL CONSTANTS ----------------------------------------------------------------
 
     public static String FILE_PATH = "FILE_PATH";
-    public static String FOOT_ID = "FOOT_ID";
 
     //endregion
 
@@ -64,44 +64,6 @@ public class MeasurementFragment extends Fragment {
 
     //region LIFECYCLE METHODS ---------------------------------------------------------------------
 
-    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 2;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
-
-    public static Bitmap decodeSampledBitmap(String filePath, int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(filePath, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(filePath, options);
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -111,45 +73,25 @@ public class MeasurementFragment extends Fragment {
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        viewUnbinder.unbind();
-    }
-
-    @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
         Bundle args = getArguments();
         if (args != null) {
-
-
             String filePath = args.getString(FILE_PATH);
+
             LayoutListener layoutListener = new LayoutListener(filePath);
             footImage.getViewTreeObserver().addOnGlobalLayoutListener(layoutListener);
-
-   /*       BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inJustDecodeBounds = true;
-            BitmapFactory.decodeFile(filePath, options);
-            int imageHeight = options.outHeight;
-            int imageWidth = options.outWidth;
-            String imageType = options.outMimeType;
-
-            int scaleValue = calculateInSampleSize(options, imageWidth, imageHeight);*/
-
-            //Bitmap myBitmap = decodeSampledBitmap(filePath, 2048, 2048);
-            //footImage.setImageBitmap(myBitmap);
-
-
-            //setFileToImageView(filePath);
-            //Bitmap myBitmap = BitmapFactory.decodeFile(filePath);
-            //myBitmap = bitmapResize(myBitmap);
-            //footImage.setImageBitmap(myBitmap);
         }
 
     }
 
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        viewUnbinder.unbind();
+    }
 
     //endregion
 
@@ -160,47 +102,20 @@ public class MeasurementFragment extends Fragment {
 
     private class LayoutListener implements ViewTreeObserver.OnGlobalLayoutListener {
 
-        String fileName;
+        String filePath;
 
-        public LayoutListener(String fileName) {
-            this.fileName = fileName;
+        public LayoutListener(String filePath) {
+            this.filePath = filePath;
         }
 
         @Override
         public void onGlobalLayout() {
-            int width = footImage.getWidth();
-            int height = footImage.getHeight();
-            int orientation = ExifInterface.ORIENTATION_NORMAL;
-
-            try {
-                ExifInterface exif = new ExifInterface(fileName);
-                orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
-                Log.i(this.getClass().getSimpleName(), "orientation: " + orientation);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            Matrix matrix = new Matrix();
-
-            switch (orientation) {
-                case ExifInterface.ORIENTATION_ROTATE_90:
-                    matrix.postRotate(90);
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_180:
-                    matrix.postRotate(180);
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_270:
-                    matrix.postRotate(270);
-                    break;
-            }
-
-            Bitmap myBitmap = decodeSampledBitmap(fileName, width, height);
-            myBitmap = Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight(), matrix, true); // rotating bitmap
-            footImage.setImageBitmap(myBitmap);
-
-            //Bitmap myBitmap = BitmapFactory.decodeFile(fileName);
             getView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
 
+            int width = footImage.getWidth();
+            int height = footImage.getHeight();
+
+            setupImageView(filePath, width, height, footImage);
         }
     }
 
@@ -220,6 +135,128 @@ public class MeasurementFragment extends Fragment {
     //endregion
 
     //region PRIVATE METHODS -----------------------------------------------------------------------
+
+
+    /**
+     * Loads the given image located in the filePath into the given ImageView, scaled to fit within the width and height area
+     * of the imageView
+     *
+     * @param filePath  Path to the image file to load
+     * @param width     The width of the ImageView (use getViewTreeObserver on ImageView)
+     * @param height    The height of the ImageView (use getViewTreeObserver on ImageView)
+     * @param imageView The ImageView to load the bitmap into
+     */
+    private void setupImageView(final String filePath, final int width, final int height, final ImageView imageView) {
+
+        // Do all bitmap calculations on a simple background thread
+        new AsyncTask<Void, Void, Boolean>() {
+
+            Bitmap myBitmap = null;
+
+            @Override
+            protected Boolean doInBackground(Void... params) {
+
+                //Determine if image needs to be rotated by looking at Exif information
+                int orientation = ExifInterface.ORIENTATION_NORMAL;
+
+                try {
+                    ExifInterface exif = new ExifInterface(filePath);
+                    orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    Log.i(this.getClass().getSimpleName(), "orientation: " + orientation);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // decode the bitmap with scaling
+                myBitmap = decodeSampledBitmap(filePath, width, height);
+
+                // rotate the bitmap if necessary
+                if (orientation != ExifInterface.ORIENTATION_NORMAL) {
+
+                    Matrix matrix = new Matrix();
+                    switch (orientation) {
+                        case ExifInterface.ORIENTATION_ROTATE_90:
+                            matrix.postRotate(90);
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_180:
+                            matrix.postRotate(180);
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_270:
+                            matrix.postRotate(270);
+                            break;
+                    }
+
+                    myBitmap = Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight(), matrix, true); // rotating bitmap
+                }
+
+                if (myBitmap != null) {
+                    return true;
+                }
+
+                return false;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                if (result) {
+                    imageView.setImageBitmap(myBitmap);
+                }
+            }
+        }.execute();
+    }
+
+
+    /**
+     * Determine a sample size based on options derived from the BitmapFactory (using inJustDecodeBounds)
+     *
+     * @param options   Options derived from a BitmapFactory call on bitmap
+     * @param reqWidth  desired width of image after scaling
+     * @param reqHeight desired height of image after scaling
+     * @return a sample size to use for the BitmapFactory
+     */
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
+    }
+
+    /**
+     * decode the bitmap to determine it's full height / width
+     *
+     * @param filePath  Path to the file
+     * @param reqWidth  desired max width
+     * @param reqHeight desired max height
+     * @return a scaled bitmap using the desired width / height
+     */
+    private static Bitmap decodeSampledBitmap(String filePath, int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
 
 
     //endregion
